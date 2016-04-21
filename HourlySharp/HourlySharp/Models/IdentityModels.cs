886bc6ae7ca57fbbc52a53285a3abdedc1bcc46a
Литвинюк Виталий <stawr.93@gmail.com>
2016-04-21 20:34:22 +0600

    1ое Собрание Добавили базового пользователя и немного заказов

--- HourlySharp/HourlySharp/Models/IdentityModels.cs
+++ HourlySharp/HourlySharp/Models/IdentityModels.cs
@@ -6,9 +6,25 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace HourlySharp.Models
 {
+    using System.Collections.Generic;
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        //First and Last Name
+        public string userFirstName { get; set; }
+        public string userLastName { get; set; }
+
+        //Phone Number
+        public string userPhoneNumber { get; set; }
+
+        //reputation
+        public float userRep { get; set; }
+
+        //User's orders
+        public virtual ICollection<Order> UserOrders { get; set; }
+
+        //
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
---
+++ HourlySharp/HourlySharp/Models/Order.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HourlySharp.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+
+        public enum OrderSpeciality
+        {
+           /// <summary>
+           /// Сантехник
+           /// </summary>
+           Plumber,
+           /// <summary>
+           /// Электрик
+           /// </summary>
+           Electrician,
+           
+           /// <summary>
+           /// Слесарь
+           /// </summary>
+            
+           Locksmith,
+
+           /// <summary>
+           /// Монтажник
+           /// </summary>
+
+           Mounter,
+
+           /// <summary>
+           /// Айтишник
+           /// </summary>
+
+           SysAdmin,
+
+           /// <summary>
+           /// Уборщик
+           /// </summary>
+
+           Cleaner,
+
+           /// <summary>
+           /// Разнорабочий
+           /// </summary>
+
+           GeneralLaborer,
+            
+
+        }
+
+
+        public string OrderComment { get; set; }
+
+ 
+
+
+    }
+}
