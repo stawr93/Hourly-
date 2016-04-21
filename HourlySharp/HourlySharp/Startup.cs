85d7a58c1fce9eda649593d589a7d8e4cc0ec2c6
Литвинюк Виталий <stawr.93@gmail.com>
2016-04-21 19:41:53 +0600

    The begining

---
+++ HourlySharp/HourlySharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HourlySharp.Startup))]
+namespace HourlySharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
